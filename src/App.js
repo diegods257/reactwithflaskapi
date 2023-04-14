843b5b43d415ed2cbae64ac2d17b1c89fb14d517
diegods257 <diegoct1855@gmail.com>
2023-04-13 20:57:33 -0500

    act7

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <Navbar />
       <div className=" p-4">
         <Routes>
-          <Route exact path="/" element={<Users />} />
+          <Route exact path="/reactwithflaskapi" element={<Users />} />
         </Routes>
       </div>
     </Router>
